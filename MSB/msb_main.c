5427ef564d230b15ce61cf29e879b0fa5dcc943b
Yipeng (Andrew) Wang <98450999+YPAndrew0907@users.noreply.github.com>
2024-02-22 18:37:35 -0500

    testing commit

--- MSB/msb_main.c
+++ MSB/msb_main.c
@@ -3,13 +3,18 @@
 #include "api/msb_can.h"
 #include "cmsis_os.h"
 
+//Thread ID identifies the thread.
 osThreadId_t canTaskHandle;
 
 void init_msb() {
-    // spawn CAN output thread
-    start_can_publisher();
+    // Create a thread to run start_can_publisher
+    canTaskHandle = osThreadNew(start_can_publisher, NULL, NULL);
+    if (canTaskHandle == NULL) {
+        // Handle error, need implementation in the future
+    }
 }
 
+// test
 void start_msb() {
 
     init_msb();
@@ -21,5 +26,9 @@ void start_msb() {
 }
 
 void stop_msb() {
-    
+    if (canTaskHandle != NULL) {
+        osThreadTerminate(canTaskHandle);
+        //put Null for now, implementation in the future
+        canTaskHandle = NULL;
+    }
 }
