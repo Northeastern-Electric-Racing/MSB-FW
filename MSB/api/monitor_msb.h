42b44ee5fb9503f99917f37d09017d6d33d57b89
caiodasilva2005 <caiodasilva05@outlook.com>
2024-02-22 20:36:37 -0500

    Fixed issues with monitor functions and threads

--- MSB/api/monitor_msb.h
+++ MSB/api/monitor_msb.h
@@ -5,8 +5,8 @@
 #include "msb_knuckle.h"
 #include "msb_central.h"
 
-void monitor_temp_msb(msb_temp_t* msb, temp_data_t* out);
-void monitor_knuckle_msb(msb_knuckle_t* msb, knuckle_data_t* out);
-void monitor_central_msb(msb_central_t* msb, central_data_t* msb_central_data);
+void monitor_temp_msb(void* arg);
+void monitor_knuckle_msb(void* arg);
+void monitor_central_msb(void* arg);
 
 #endif
