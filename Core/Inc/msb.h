e56c71555a9ff11de6899df09208033953558b49
Myers-Ty <myers.ty@northeastern.edu>
2024-12-05 20:45:00 -0500

    Clang format

--- Core/Inc/msb.h
+++ Core/Inc/msb.h
@@ -33,7 +33,8 @@ int8_t debug2_write(bool status);
 
 int8_t vcc5_en_write(bool status);
 
-int32_t imu_data_get(stmdev_ctx_t *ctx, stmdev_ctx_t *aux_ctx, lsm6dso_md_t *imu_md_temp, lsm6dso_data_t *imu_data_temp);
+int32_t imu_data_get(stmdev_ctx_t *ctx, stmdev_ctx_t *aux_ctx,
+		     lsm6dso_md_t *imu_md_temp, lsm6dso_data_t *imu_data_temp);
 
 #ifdef SENSOR_SHOCKPOT
 void shockpot_read(uint32_t shockpot_sense);
