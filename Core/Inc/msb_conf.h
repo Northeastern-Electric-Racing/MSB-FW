4d5a4530bc39fb44235338ec872bc06aa9c400b0
Kaushik Mehta <mehta.kau@northeastern.edu>
2025-01-31 13:24:13 -0500

    Fixed formatting

--- Core/Inc/msb_conf.h
+++ Core/Inc/msb_conf.h
@@ -5,11 +5,11 @@
 
 // MONITOR
 #define DELAY_TEMP_SENSOR_REFRESH 500
-#define DELAY_IMU_REFRESH 500
-#define DELAY_SHOCKPOT_REFRESH 500
-#define DELAY_STRAIN_REFRESH 500
-#define DELAY_TOF_REFRESH 500
-#define DELAY_WHEEL_TEMP_REFRESH 500
+#define DELAY_IMU_REFRESH         500
+#define DELAY_SHOCKPOT_REFRESH    500
+#define DELAY_STRAIN_REFRESH      500
+#define DELAY_TOF_REFRESH         500
+#define DELAY_WHEEL_TEMP_REFRESH  500
 
 // CONTROLLER
 #define DELAY_DEBUG_LED_REFRESH 250
@@ -17,13 +17,13 @@
 #define DELAY_CAN_DISPATCH 2
 
 // CAN IDS
-#define CANID_TEMP_SENSOR 0x602
-#define CANID_IMU_ACCEL 0x603
-#define CANID_IMU_GYRO 0x604
-#define CANID_STRAIN_SENSE 0x605
-#define CANID_SHOCK_SENSE 0x606
-#define CANID_TOF 0x607
-#define CANID_WHEEL_TEMP 0x608
+#define CANID_TEMP_SENSOR     0x602
+#define CANID_IMU_ACCEL       0x603
+#define CANID_IMU_GYRO        0x604
+#define CANID_STRAIN_SENSE    0x605
+#define CANID_SHOCK_SENSE     0x606
+#define CANID_TOF             0x607
+#define CANID_WHEEL_TEMP      0x608
 #define CANID_IMU_ORIENTATION 0x609
 
 // Sensors to use, comment out to disable
@@ -49,11 +49,11 @@
 
 // Note
 // STACK SIZES
-#define CAN_DISPATCH_STACK_SIZE 128 * 8    // can_dispatch_handle
-#define LED_CONTROLLER_STACK_SIZE 32 * 8   // led_controller_handle
-#define DEFAULT_TASK_STACK_SIZE 128 * 4    // defaultTaskHandle
-#define TEMP_MONITOR_STACK_SIZE 64 * 16    // temp_monitor_handle
-#define IMU_MONITOR_STACK_SIZE 128 * 32    // imu_monitor_handle
-#define TOF_MONITOR_STACK_SIZE 128 * 8     // tof_monitor_handle
+#define CAN_DISPATCH_STACK_SIZE     128 * 8    // can_dispatch_handle
+#define LED_CONTROLLER_STACK_SIZE   32 * 8   // led_controller_handle
+#define DEFAULT_TASK_STACK_SIZE     128 * 4    // defaultTaskHandle
+#define TEMP_MONITOR_STACK_SIZE     64 * 16    // temp_monitor_handle
+#define IMU_MONITOR_STACK_SIZE      128 * 32    // imu_monitor_handle
+#define TOF_MONITOR_STACK_SIZE      128 * 8     // tof_monitor_handle
 #define SHOCKPOT_MONITOR_STACK_SIZE 64 * 8 // shockpot_monitor_handle
-#define STRAIN_MONITOR_STACK_SIZE 64 * 8   // strain_monitor_handle
+#define STRAIN_MONITOR_STACK_SIZE   64 * 8   // strain_monitor_handle
